79f520e91f4b45c5389a2c3ec76c9962822ed668
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-22 18:39:14 +0500

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -3,7 +3,10 @@
 echo "[ENTRYPOINT] Starting HLS Proxy..."
 /opt/hlsp/hls-proxy -config-path /opt/hlsp/config -address 0.0.0.0 -port 8080 &
 
+# Selfping в фоне
 sleep 2
 echo "[ENTRYPOINT] Starting selfping..."
-exec /opt/selfping.sh
-while true; do sleep 300; done
+/opt/selfping.sh &
+
+# Держим контейнер живым через основной процесс — HLS Proxy
+wait
