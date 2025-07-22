733fcaa81d774834c63016d0aff34d2153e5714e
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-22 16:07:27 +0500

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# Запуск selfping.sh в фоне
-/selfping.sh &
+# Запускаем HLS Proxy в фоне
+/opt/hlsp/hls-proxy -config-path /opt/hlsp/config -address 0.0.0.0 -port 8080 &
+HLS_PID=$!
 
-# Запуск hls-proxy
-exec ./hls-proxy -config-path /opt/hlsp/config -address 0.0.0.0 -port 8080
+# Пингуем локально, чтобы прокси не заснул
+/opt/selfping.sh &
+
+# Ждём завершения основного процесса
+wait $HLS_PID
