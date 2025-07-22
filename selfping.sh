4d2e6f6e3f7cb4b45af1000538293aabe96e80c5
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-22 19:11:17 +0500

    Update selfping.sh

--- selfping.sh
+++ selfping.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Ждём запуска hls-proxy
-until curl -s --max-time 3 http://127.0.0.1:8080/health >/dev/null; do
+until curl -s --max-time 3 http://127.0.0.1:8080/status >/dev/null; do
   echo "[SELF-PING] Waiting for hls-proxy..."
   sleep 2
 done
 
 while true; do
   echo "[SELF-PING] Pinging localhost:8080"
-  curl --max-time 5 --connect-timeout 2 -s http://127.0.0.1:8080/health >/dev/null || echo "[SELF-PING] Failed"
+  curl --max-time 5 --connect-timeout 2 -s http://127.0.0.1:8080/status >/dev/null || echo "[SELF-PING] Failed"
   sleep 60
 done
