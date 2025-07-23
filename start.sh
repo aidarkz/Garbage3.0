06ae0b132230956ab8f571efa8cd44b8b306bfa5
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-23 15:36:50 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
+# Стартуем nginx в фоне
 service nginx start
 
-# Keep-alive loop (опционально, но желательно)
-while true; do
-    curl -s http://127.0.0.1:8080/health > /dev/null
-    sleep 60
-done &
+# Ждём пару секунд (иногда nginx может не успеть подняться)
+sleep 2
 
+# Стартуем hls-proxy как основной процесс (без &!)
 exec /opt/hlsp/hls-proxy -address 0.0.0.0 -port 8080
