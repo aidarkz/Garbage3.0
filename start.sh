9b0eba34dec413bf03cae7f85f96089c1a15544f
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-23 16:26:26 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ echo "[INFO] Starting hls-proxy..."
 # Запуск hls-proxy в фоне
 /opt/hlsp/hls-proxy -address 0.0.0.0 -port 8080 &
 
-# PID отслеживание
 HLS_PROXY_PID=$!
 
-# ТAIL-лог если stdout есть, либо просто keep-alive цикл
+# Keep-alive цикл
 echo "[INFO] hls-proxy запущен с PID: $HLS_PROXY_PID"
 while kill -0 "$HLS_PROXY_PID" 2>/dev/null; do
-    sleep 5
+    curl -s http://127.0.0.1:8080/health > /dev/null
+    sleep 30
 done
 
 echo "[ERROR] hls-proxy завершился"
