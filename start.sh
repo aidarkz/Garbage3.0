55cbaf623441fd54ccb2807e2d37557230080466
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-23 19:03:12 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-LOG_FILE="/opt/hlsp/logs/proxy.log"
-echo "[INIT] Starting HLS proxy at $(date)" >> "$LOG_FILE"
+echo "[start.sh] Запуск HLS Proxy..."
+/opt/hlsp/hls-proxy -address 0.0.0.0 -port 8080
 
-# Убедимся, что лог-файл существует
-mkdir -p /opt/hlsp/logs
-touch "$LOG_FILE"
-
-# Запуск hls_proxy в foreground и логирование
-exec /opt/hlsp/hls-proxy >> "$LOG_FILE" 2>&1
+# Предотвращаем завершение контейнера
+echo "[start.sh] Контейнер будет оставаться живым"
+tail -f /dev/null
