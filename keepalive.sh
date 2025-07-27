b66740165073abeffa51f3cf0a546757ca186645
aidarkz <52048081+aidarkz@users.noreply.github.com>
2025-07-27 11:50:34 +0500

    Update and rename start2.sh to keepalive.sh

---
+++ keepalive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+    curl -s http://127.0.0.1:8282/health > /dev/null
+    curl -s http://www.google.com > /dev/null
+    sleep 60
+done
--- start2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "[start.sh] Старт supervisor и cron"
-service cron start
-supervisord -c /etc/supervisor/conf.d/supervisor.conf
-tail -f /dev/null
